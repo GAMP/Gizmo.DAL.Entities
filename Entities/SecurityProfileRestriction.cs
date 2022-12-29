f974db29be7ce5c74f79acfa5dc6eaea1d0175fd
amromar-dev <67632505+amromar-dev@users.noreply.github.com>
2022-12-29 12:34:09 +0200

    Remove unsued usings

--- Entities/SecurityProfileRestriction.cs
+++ Entities/SecurityProfileRestriction.cs
@@ -1,12 +1,7 @@
 using CoreLib;
-using CyClone;
-using Gizmo.DAL.Entities;
 using ProtoBuf;
-using SharedLib;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
 
 namespace GizmoDALV2.Entities
