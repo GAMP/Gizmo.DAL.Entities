cecc147451436b75928c8e6a121e8167c42fc805
Oleg Savelos <olegsavelos@gmail.com>
2023-10-09 19:08:44 +0300

    added branch entity.

---
+++ Entities/Branch.cs
@@ -0,0 +1,41 @@
+﻿using GizmoDALV2;
+using GizmoDALV2.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Branch entity.
+    /// </summary>
+    public class Branch : ModifiableByOperatorBase, IEnabled, IDeletable
+    {
+        #region CONSTRUCTOR
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public Branch() : base()
+        { } 
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Gets or sets branch name.
+        /// </summary>
+        [StringLength(SQLStringSize.TINY)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets if branch is default.
+        /// </summary>
+        public bool IsDefault { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsEnabled { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsDeleted { get; set; }
+
+        #endregion
+    }
+}
