d6a4257139f66da9f6f1d77efc8000310768d34d
Oleg Savelos <olegsavelos@gmail.com>
2024-01-25 20:09:45 +0200

    operator branch entity.

---
+++ Entities/UserOperatorBranch.cs
@@ -0,0 +1,33 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Operator branch entity.
+    /// </summary>
+    public sealed class UserOperatorBranch : ModifiableByOperatorBase
+    {
+        /// <summary>
+        /// Gets or sets operator id.
+        /// </summary>
+        public int OperatorId { get; set; }
+
+        /// <summary>
+        /// Gets or sets branch id.
+        /// </summary>
+        public int BranchId { get; set; }
+
+        /// <summary>
+        /// Gets or sets default.
+        /// </summary>
+        public bool IsDefault { get; set; }
+
+        /// <summary>
+        /// Gets or sets operator.
+        /// </summary>
+        public UserOperator Operator { get; set; }
+
+        /// <summary>
+        /// Gets or sets branch.
+        /// </summary>
+        public Branch Branch { get; set; }       
+    }
+}
