1bb74228129664496b0c3cd54cd81a4345810dee
Oleg Savelos <olegsavelos@gmail.com>
2024-01-25 20:09:55 +0200

    generate new guid.

--- Entities/Branch.cs
+++ Entities/Branch.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,14 +10,16 @@ namespace Gizmo.DAL.Entities
     /// Branch entity.
     /// </summary>
     [NotMapped()]
-    public class Branch : ModifiableByOperatorBase, IEnabled, IDeletable
+    public class Branch : ModifiableByOperatorBase, IEnabled, IDeletable, IReplicatable
     {
         #region CONSTRUCTOR
         /// <summary>
         /// Creates new instance.
         /// </summary>
         public Branch() : base()
-        { }
+        {
+            Guid = Guid.NewGuid();
+        }
         #endregion
 
         #region PROPERTIES
@@ -25,12 +28,59 @@ namespace Gizmo.DAL.Entities
         /// Gets or sets branch name.
         /// </summary>
         [StringLength(SQLStringSize.TINY)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         /// <summary>
-        /// Gets or sets if branch is default.
+        /// Gets or sets branch city.
         /// </summary>
-        public bool IsDefault { get; set; }
+        public string? City { get; set; }
+
+        /// <summary>
+        /// Gets or sets address.
+        /// </summary>
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets contact phone.
+        /// </summary>
+        public string? ContactPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets email.
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Gets to sets postal code.
+        /// </summary>
+        public string? PostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets region.
+        /// </summary>
+        public string? Region { get; set; }
+
+        /// <summary>
+        /// Gets or sets web site.
+        /// </summary>
+        public string? WebSite { get; set; }
+
+        /// <summary>
+        /// Gets or sets info.
+        /// </summary>
+        public string? Info { get; set; }
+
+
+        //Guid add guid
+        //City add city (45 char)
+        //Address add address (255 char)
+        //ContactPhone phone ? contact phone mobile or landline (45 char)
+        //Email email contact
+        //ZipCode zip code ? (alphanumeric)
+        //Region region (255 chars)
+        //GeoLocation geo location
+        //WebSite url
+        //Info (255 char)
 
         /// <inheritdoc/>
         public bool IsEnabled { get; set; }
@@ -42,6 +92,11 @@ namespace Gizmo.DAL.Entities
         /// Gets or sets time zone.
         /// </summary>
         public string? TimeZone { get; set; }
+
+        /// <summary>
+        /// Gets or sets replication guid.
+        /// </summary>
+        public Guid Guid { get; set; }
 
         #endregion
     }
