63f3d28650dd8d74e1cc6ac66973e84b1891fa90
Oleg Savelos <olegsavelos@gmail.com>
2024-11-02 14:38:30 +0200

    Add optional branch interface.

---
+++ Interfaces/Entities/IBranchedOptionalEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Optional branch entity.
+    /// </summary>
+    /// <remarks>
+    /// Represents an entity with optional branch.
+    /// </remarks>
+    public interface IBranchedOptionalEntity
+    {
+        /// <summary>
+        /// Gets or sets branch id.
+        /// </summary>
+        public int? BranchId { get; set; }
+
+        /// <summary>
+        /// Gets branch navigation entity.
+        /// </summary>
+        public Branch? Branch { get; }
+    }
+}
