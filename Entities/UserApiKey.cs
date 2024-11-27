5a96f76446f0b413b98f280a27357de13106b308
Oleg Savelos <olegsavelos@gmail.com>
2024-11-27 10:21:47 +0200

    updates.

---
+++ Entities/UserApiKey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// User api key entity.
+    /// </summary>
+    public sealed class UserApiKey : UserOperator
+    {
+        /// <summary>
+        /// Gets or sets the api key.
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the api key type.
+        /// </summary>
+        public ApiKeyType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the api key expire time.
+        /// </summary>
+        public DateTime? ExpireTime { get; set; }
+    }
+}
---
+++ Enumerations/ApiKeyType.cs
@@ -0,0 +1,24 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Api key type.
+    /// </summary>
+    public enum ApiKeyType
+    {
+        /// <summary>
+        /// Unrestricted.
+        /// </summary>
+        /// <remarks>
+        /// Such api key will have all permissions by default.
+        /// </remarks>
+        Unrestricted = 0,
+
+        /// <summary>
+        /// Restricted.
+        /// </summary>
+        /// <remarks>
+        /// Such api key will have only permissions explicitly granted.
+        /// </remarks>
+        Restricted = 1
+    }
+}
