cbafe65289200ba5697140994e55d904957db801
Oleg Savelos <olegsavelos@gmail.com>
2024-11-28 17:33:08 +0200

    add preset topup.

---
+++ Entities/PresetTopUp.cs
@@ -0,0 +1,13 @@
+﻿using SharedLib;
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Preset top up.
+    /// </summary>
+    public sealed class PresetTopUp : ValueBase<decimal>, IDisplayOrder
+    {
+        ///<inheritdoc/>
+        public int DisplayOrder { get; set; }
+    }
+}
