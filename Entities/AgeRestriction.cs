b677e4eede9a227a4a74e54b9f6dceccaf3250fd
Oleg Savelos <olegsavelos@gmail.com>
2024-11-28 15:56:59 +0200

    updates.

---
+++ Entities/AgeRestriction.cs
@@ -0,0 +1,28 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Age restriction.
+    /// </summary>
+    public abstract class AgeRestriction : CreatedByOperatorBase
+    {
+        /// <summary>
+        /// Age from.
+        /// </summary>
+        public int AgeFrom { get; init; }
+
+        /// <summary>
+        /// Age to.
+        /// </summary>
+        public int AgeTo { get; init; }
+
+        /// <summary>
+        /// Day minute from.
+        /// </summary>
+        public int? DayMinuteFrom { get; set; }
+
+        /// <summary>
+        /// Day minute to.
+        /// </summary>
+        public int? DayMinuteTo { get; set; }
+    }
+}
---
+++ Entities/AgeRestrictionLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Age rating login restriction.
+    /// </summary>
+    public sealed class AgeRestrictionLogin : AgeRestriction
+    {
+    }
+}
