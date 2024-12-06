258688dea4a2c5c149d264925475147c1a0a6452
Oleg Savelos <olegsavelos@gmail.com>
2024-12-06 16:27:27 +0200

    updates.

---
+++ Entities/Notification.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Notification entity.
+    /// </summary>
+    public class Notification : ModifiableByOperatorBase , IDisable
+    {
+        /// <summary>
+        /// Gets or sets notification type.
+        /// </summary>
+        public NotificationType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets notification focus type.
+        /// </summary>
+        public NotificationFocusType FocusType { get; set; }
+
+        /// <summary>
+        /// Gets or sets notification message.
+        /// </summary>
+        public string? Message { get; set; }
+
+        ///<inheritdoc/>
+        public bool IsDisabled { get; set; }   
+    }    
+}
--- Entities/NotificationTimed.cs
+++ Entities/NotificationTimed.cs
@@ -10,21 +10,6 @@ namespace Gizmo.DAL.Entities
         /// <summary>
         /// Notification minute.
         /// </summary>
-        public int Minute { get; set; }
-
-        /// <summary>
-        /// Gets or sets notification type.
-        /// </summary>
-        public NotificationType Type { get; set; }
-
-        /// <summary>
-        /// Gets or sets notification focus type.
-        /// </summary>
-        public NotificationFocusType FocusType { get; set; }
-
-        /// <summary>
-        /// Gets or sets notification message.
-        /// </summary>
-        public string? Message { get; set; }
+        public int Minute { get; set; }   
     }
 }
