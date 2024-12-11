dc99b9298fcc0c324a17e8fa5a7eb3c11066a0a3
Oleg Savelos <olegsavelos@gmail.com>
2024-12-11 16:11:56 +0200

    updates.

---
+++ Entities/PresetReservationTime.cs
@@ -0,0 +1,23 @@
+﻿using SharedLib;
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Preset reservation range entity.
+    /// </summary>
+    public sealed class PresetReservationTime : ValueBase<int> , IDisplayOrder
+    {
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public PresetReservationTime():base() { }
+
+        /// <summary>
+        /// Gets or sets display order.
+        /// </summary>
+        public int DisplayOrder
+        {
+            get; set;
+        }
+    }
+}
