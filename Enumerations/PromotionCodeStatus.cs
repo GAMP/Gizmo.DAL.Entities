3336b1dafe5117521af4fff39ddd654bbd8e1fb2
Oleg Savelos <olegsavelos@gmail.com>
2025-09-08 17:45:10 +0300

    promotion exception support.

--- Enumerations/PromotionCodeStatus.cs
+++ Enumerations/PromotionCodeStatus.cs
@@ -9,13 +9,20 @@
         /// Promotion code was not used.
         /// </summary>
         Unused = 0,
+        
         /// <summary>
         /// Promotion code is used.
         /// </summary>
         Used = 1,
+
         /// <summary>
         /// Promotion code is depleted.
         /// </summary>
         Depleted = 2,
+
+        /// <summary>
+        /// Revoked.
+        /// </summary>
+        Revoked = 3,
     }
 }
