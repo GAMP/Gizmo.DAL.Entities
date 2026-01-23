7c37352cb99606fc8230327d5c7fa1bf5140b4d8
Oleg Savelos <olegsavelos@gmail.com>
2026-01-23 19:36:06 +0200

    updates.

---
+++ Entities/Integration.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+
+using System;
+
+namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Integration entity.
+    /// </summary>
+    public sealed class Integration : CreatedByOperatorBase , IDisable , IDeletable
+    {
+        /// <summary>
+        /// Integration name.
+        /// </summary>
+        public required string Name { get; set; }
+
+        /// <summary>
+        /// Integration type guid.
+        /// </summary>
+        public Guid TypeGuid {  get; set; }
+
+        /// <summary>
+        /// Public stable id.
+        /// </summary>
+        public Guid PublicId { get; set; }
+
+        /// <summary>
+        /// Configuration json.
+        /// </summary>
+        public string? ConfigJson { get; set; }
+
+        /// <summary>
+        /// Current config schema version.
+        /// </summary>
+        public int? ConfigSchemaVersion { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsDisabled { get; set; }
+
+        /// <inheritdoc/>
+        public bool IsDeleted { get; set; }
+    }
+}
