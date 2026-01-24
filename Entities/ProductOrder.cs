62e9e702732f588b40fb1fb1ed628ae518b3e81a
Oleg Savelos <olegsavelos@gmail.com>
2026-01-24 12:01:04 +0200

    cleanup.

--- Entities/ProductOrder.cs
+++ Entities/ProductOrder.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 using ProtoBuf;
-using SharedLib;
 
 namespace Gizmo.DAL.Entities
 {
---
+++ Enumerations/OrderSource.cs
@@ -0,0 +1,23 @@
+﻿namespace Gizmo.DAL.Entities
+{
+    /// <summary>
+    /// Client order source.
+    /// </summary>
+    public enum OrderSource
+    {
+        /// <summary>
+        /// Default. (POS)
+        /// </summary>
+        Default = 0,
+        
+        /// <summary>
+        /// Client machine.
+        /// </summary>
+        Client = 1,
+
+        /// <summary>
+        /// Web portal.
+        /// </summary>
+        Web = 2,
+    }
+}
